001ba0e8933a04cf341d581d5e721d6c614b3cc3
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-01-30 20:49:06 -0500

    gg

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,45 @@
+import axios from 'axios';
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
 const Login = () => {
+
+    const { register, handleSubmit } = useForm();
+
+    const submit = (data) => {
+        console.log(data);
+axios.post()
+
+
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Form onSubmit={handleSubmit(submit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    {...register("email")}
+                />
+                <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    placeholder="Password"
+                    {...register("password")}
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 };
 
