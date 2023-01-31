220ecd23e6e01f056819e0d32ebc255477959510
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-01-30 21:25:16 -0500

    tt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,29 @@ import axios from 'axios';
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
     const { register, handleSubmit } = useForm();
 
+    const navigate = useNavigate();
+
     const submit = (data) => {
         console.log(data);
-axios.post()
+        axios.post('https://e-commerce-api-v2.academlo.tech/api/v1/users/login/', data)
+            .then((res) => {
+                localStorage.setItem("token", res.data.token);
+                navigate("/");
+            })
+            .catch((error) => {
+                if (error.response.status === 401) {
+                    alert("Credenciales incorrectas")
+                }
+                console.log(error);
+            });
 
-
-    }
+    };
 
     return (
         <Form onSubmit={handleSubmit(submit)}>
