a67fa208fd5d2cbe0e5a4ee8763849225005607e
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-02-02 18:50:53 -0500

    oo

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { filterProductsCategoryThunk } from '../store/slices/products.slice';
 
 
 const Products = () => {
-    const {id} = useParams();
+    const { id } = useParams();
+    const [products, setProducts] = useState({});
+
+    const productsList = useSelector(state => state.products);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        axios.get(`https://e-commerce-api-v2.academlo.tech/api/v1/products/${id}/`)
+            .then(res => {
+                setProducts(res.data)
+                dispatch(filterProductsCategoryThunk(res.data.category.id))    
+                
+            });
+    }, [])
+
+    console.log(products);
+
     return (
         <div>
-            <h1>mostrando Product<b>{id}</b></h1>
+            <h1>{products.title}<b>{id}</b></h1>
+            <h3>{products.brand}</h3>
+            <img src={products.images?.[0].url} alt="" />
+            <p>{products.description}</p>
+            <p>${products.price}</p>
+
+
+{
+    productsList.map(productsItem =>(
+        <li key={productsItem.id}>
+            {productsItem.title}
+            {productsItem.brand}
+            <img src={productsItem.images?.[0].url} alt="" />
+            ${productsItem.price}
+            </li>
+            
+    ))
+}
+
         </div>
     );
 };
