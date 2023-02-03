a666a8d474d54a400cba509b61facf5ee1cb7d05
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-02-02 22:54:29 -0500

    oo

--- src/pages/Purchases.jsx
+++ src/pages/Purchases.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPurchasesThunk } from '../store/slices/purchases.slice';
 
 
 const Purchases = () => {
-    const purchases = useSelector(state => state.purcharses);
-    console.log(purchases)
+    
+    const purchases = useSelector(state => state.purchases);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPurchasesThunk());
+
+    }, [])
+console.log(purchases)
+
 
     return (
         <div>
             <h1>hello</h1>
+            <ul>
+                {purchases.map(purchases =>(
+                    <li>{purchases.products.title}</li>
+                ))}
+            </ul>
         </div>
     );
 };
