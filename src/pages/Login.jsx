302dc45e07f46d05e09d3b1be6f5b89c8f79ce62
Miguel1488 <118023734+Miguel1488@users.noreply.github.com>
2023-03-08 17:39:25 -0500

    s

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
 
             <h1>Welcome! Enter your email and password to continue</h1>
             <div><h4> email: john@gmail.com</h4> </div>
-            <div><h4>password: 1234</h4></div>
+            <div><h4>password: john1234</h4></div>
 
 
             <Form.Group className="mb-3" controlId="formBasicEmail">
